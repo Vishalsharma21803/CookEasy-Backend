e6942aefb3186a7713e06c56cb426c8a978e8a42
Vishal Sharma <vishal2003ggg@gmail.com>
2025-07-11 11:04:01 +0530

    fix: use process.env.PORT for deployment

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGO_URL,{
 })  
 
 
-
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log("server is running on port: ",PORT);
 });
